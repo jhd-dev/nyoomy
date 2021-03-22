d722d4302649d26ae9b71dda53eb281b71e607e7
jhd-dev <doliverjonathan@gmail.com>
2021-03-22 02:56:27 -0400

    added deleteUser and updatePassword mutations

--- src/server/model/mutations/UserMutation.ts
+++ src/server/model/mutations/UserMutation.ts
@@ -1,6 +1,7 @@
 import { UserType } from "../types/UserType";
-import { GraphQLString } from 'graphql';
+import { GraphQLString, GraphQLID } from 'graphql';
 import { Users } from '../entities/Users';
+import { MessageType } from '../types/MessageType';
 
 export const CREATE_USER = {
     type: UserType,
@@ -13,5 +14,38 @@ export const CREATE_USER = {
         const { name, username, password } = args;
         await Users.insert({ name, username, password });
         return args;
+    },
+};
+
+export const DELETE_USER = {
+    type: MessageType,
+    args: {
+        id: { type: GraphQLID },
+    },
+    async resolve(_parent: any, args: any) {
+        const id = args.id;
+        await Users.delete(id);
+        return { success: true, message: "Successfully deleted user." };
+    },
+};
+
+export const UPDATE_PASSWORD = {
+    type: MessageType,
+    args: {
+        username: { type: GraphQLString },
+        oldPassword: { type: GraphQLString },
+        newPassword: { type: GraphQLString },
+    },
+    async resolve(_parent: any, args: any) {
+        const { username, oldPassword, newPassword } = args;
+
+        const user = await Users.findOne({ username });
+        if (!user) throw new Error("User does not exist.");
+
+        const actualPassword = user?.password;
+        if (oldPassword !== actualPassword) throw new Error("Incorrect password.")
+
+        await Users.update({ username }, { password: newPassword });
+        return { success: true, message: "Successfully changed password." };
     },
 };
