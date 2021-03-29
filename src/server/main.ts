ce1e6cc980b2ecaa4501d371be4f3ac224f7091e
jhd-dev <doliverjonathan@gmail.com>
2021-03-29 19:42:40 -0400

    removed testing from server entry

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,28 +1,4 @@
-
 import AppServer from "./AppServer";
-import { NODE_ENV, PORT } from "./config/env";
-import Jasmine from "jasmine";
+import { PORT } from './config/env';
 
-main();
-
-function main(): void {
-    switch(NODE_ENV) {
-        case "production":
-        case "development":
-            startServer();
-            break;
-        case "test":
-        case "testing":
-            runTests();
-            break;
-        default:
-            console.error(`NODE_ENV "${NODE_ENV}" not recognized.`);
-    }
-}
-
-function startServer(): void {
-    new AppServer().start( PORT );
-}
-
-function runTests(): void {
-}
+new AppServer().start( PORT );
